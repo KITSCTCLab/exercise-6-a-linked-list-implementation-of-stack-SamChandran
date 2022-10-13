2cdb95fd4f96098f1f8006bfe959d1b1f34b091b
SamChandran <91770355+SamChandran@users.noreply.github.com>
2022-10-13 11:16:32 +0530

    initial commit 2

--- Main.py
+++ Main.py
@@ -2,36 +2,39 @@ class Node:
   def __init__(self, data):
     self.data = data
     self.next = None
-    
+
 
 class Stack:
   def __init__(self):
     self.head = None
 
   def push(self, data) -> None:
-    new = Node(data)
-    if not self.head== None:
-      new.next = self.head
-      self.head=new
-    else:
-      self.head=new
-      
+    # Write your code here
+    new_node = Node(data)
+    new_node.next = self.head
+    self.head = new_node
 
   def pop(self) -> None:
-    if not self.head==None:
-      self.head = self.head.next
+    # Write your code here
+    if(self.head != None): 
+      temp = self.head
+      self.head = temp.next
+      temp.next = None
 
   def status(self):
     """
     It prints all the elements of stack.
     """
-    # Write your code here  
-    ele= ""
-    temp=self.head
-    while temp is not None:
-      ele += str(temp.data) + "=>"
-      temp=temp.next
-    print (ele+"None")
+    # Write your code here 
+    if(self.head != None):
+      temp = self.head
+      while(temp.next != None):
+        print(temp.data, end = "=>")
+        temp = temp.next
+      else:
+        print(temp.data,"None", sep="=>")
+    else:
+      print("None")
 
 
 # Do not change the following code
